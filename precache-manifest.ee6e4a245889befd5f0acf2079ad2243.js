344d0e5bce70325a76e1d29f01aeeb81850116c6
Daland Dixon <dalanddixon@Dalands-MBP.fios-router.home>
2019-01-11 09:01:24 -0500

    Updates

---
+++ precache-manifest.ee6e4a245889befd5f0acf2079ad2243.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "560d0240dbd1ded46208",
+    "url": "/clicky-game/static/js/main.560d0240.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/clicky-game/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "794c23ff0ef7e0192e6cafeb28f03d23",
+    "url": "/clicky-game/index.html"
+  }
+];
