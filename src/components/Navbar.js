7f341aa81912c36cbeaf3b72746232156ce2523a
Daland Dixon <dalanddixon@Dalands-MBP.fios-router.home>
2019-03-12 14:13:04 -0400

    Replace Shoto Photo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,15 @@
 import React from "react";
 
 const Navbar = (props) => (
-    <div>
-        <nav className="navbar sticky-top navbar-light bg-light">
-            <div className="container">
-                <span className="navbar-brand mb-0 h1">Clicky Game</span>
-                <span className="nav justify-content-center">Click an image to begin!</span>
-                <span className="nav justify-content-end">Score: {props.score} | Top Score: {props.topScore} </span>
-            </div>
-        </nav>
-    </div>
+  <div>
+    <nav className="navbar sticky-top navbar-light bg-light">
+      <div className="container">
+        <span className="navbar-brand mb-0 h1">Clicky Game</span>
+        <span className="nav justify-content-center">Click an image to begin!</span>
+        <span className="nav justify-content-end">Score: {props.score} | Top Score: {props.topScore} </span>
+      </div>
+    </nav>
+  </div>
 );
 
 export default Navbar;
